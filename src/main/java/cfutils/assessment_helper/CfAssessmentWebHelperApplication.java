74ea0317bd4ca23e53b900d60b17b09f76c3cb44
Azorious07 <133042335+Azorious07@users.noreply.github.com>
2026-04-14 20:02:16 +0300

    Initial commit

---
+++ src/main/java/cfutils/assessment_helper/CfAssessmentWebHelperApplication.java
@@ -0,0 +1,13 @@
+package cfutils.assessment_helper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfAssessmentWebHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CfAssessmentWebHelperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cfutils/assessment_helper/CfAssessmentWebHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package cfutils.assessment_helper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CfAssessmentWebHelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
